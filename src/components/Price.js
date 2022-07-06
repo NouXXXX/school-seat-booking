ae09028f30d585d68b2efad32f32d99960df74b4
NouXXXX <leedh50778037@gmail.com>
2022-07-06 22:13:06 +0900

    price message edited

--- src/components/Price.js
+++ src/components/Price.js
@@ -5,7 +5,7 @@ export default function Price() {
 	const {seats} = useContext(BookContext)
 	return (
 		<div>
-			<p>Selected {seats.totalSeats} seats and the total price is {seats.totalSeats * seats.seatPrice}Won</p>
+			<p>선택된 자리는 {seats.totalSeats} 좌석이고, 총 가격은 {seats.totalSeats * seats.seatPrice}Won 입니다.</p>
 		</div>
 	)
 }
